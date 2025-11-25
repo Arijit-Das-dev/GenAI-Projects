57478ca1848e96e716930d8ccd7eefc5b107372e
Arijit-Das-Dev <arijitdas.das99@gmail.com>
2025-11-25 20:25:15 +0530

    Delete unwanted files from Second Project folder

--- MergePages.py
+++ MergePages.py
@@ -1,29 +1,21 @@
 import streamlit as st
 
-project5_page = st.Page(
-    
-        page="Second Project/LoginPage.py",
-        title="Login/Signup",
-        icon=":material/login:",
-        default = True
-
+home_page = st.Page(
+    page="Second Project/About_Page.py",
+    title="Home",
+    icon=":material/account_circle:",
+    url_path="about"
 )
 
-About_page = st.Page(
-
-        page ="Second Project/About_Page.py",
-        title = "Home",
-        icon = ":material/account_circle:",
-
+chat_page = st.Page(
+    page="Second Project/Chat_with_AI.py",
+    title="Start Chat",
+    icon=":material/smart_toy:",
+    url_path="chat"
 )
 
-Project1_page = st.Page(
-
-        page = "Second Project/Chat_with_AI.py",
-        title = "Start Chat",
-        icon = ":material/smart_toy:"
-)
+pages = [home_page, chat_page]
 
 
-pg = st.navigation(pages=[project5_page, About_page, Project1_page])
+pg = st.navigation(pages=pages)
 pg.run()
